985c648fd1791d2de7dcd48164ff95516d8e8d58
Chris Chandler <chris@hartogjacobs.com>
2025-10-23 12:43:15 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+require_once 'Fruit.php';
+
+$apple = new Fruit();
+$apple->set_name('Apple');
+
+echo $apple->get_name();
+/// test
+?>
+</body>
+</html>
+</body>
+</html>
